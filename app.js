964faee099b026938f19575063c0389bbf0944e6
wahid6313 <wahidali8566@gmail.com>
2024-08-30 18:58:27 +0530

    handling-post-request

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
+app.use(express.json());
+// app.get("/", (req, res) => {
+//   res.status(200).send("hello world");
+// });
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello world");
+// app.post("/", (req, res) => {
+//   res.json({ name: "wahid ali", country: "India" });
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/wahid.json`));
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "succes",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post("/", (req, res) => {
-  res.json({ name: "wahid ali", country: "India" });
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+  res.send("done");
 });
 
 const port = 3000;
