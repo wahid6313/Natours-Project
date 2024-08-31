066c2c04ba14052ddaa4acf8850f6534aa87277e
wahid6313 <wahidali8566@gmail.com>
2024-08-31 12:37:40 +0530

    All Routes

--- app.js
+++ app.js
@@ -3,17 +3,10 @@ const fs = require("fs");
 
 const app = express();
 app.use(express.json());
-// app.get("/", (req, res) => {
-//   res.status(200).send("hello world");
-// });
-
-// app.post("/", (req, res) => {
-//   res.json({ name: "wahid ali", country: "India" });
-// });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/wahid.json`));
 
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "succes",
     results: tours.length,
@@ -21,12 +14,63 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const getTour = (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "'Invalid Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "succes",
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
   console.log(req.body);
   res.send("done");
-});
+};
+
+const updateTour = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "< Updated tour Here>",
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
+
+// app.get("/api/v1/tours", getAllTours);
+// app.get("/api/v1/tours/:id", getTour);
+// app.post("/api/v1/tours", createTour);
+// app.patch("/api/v1/tours/:id", updateTour);
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
