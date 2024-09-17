a245b24617561c20adfb785067e8e5286fb99278
wahid6313 <wahidali8566@gmail.com>
2024-09-17 12:35:32 +0530

    Error-Control

--- Controller/errorController.js
+++ Controller/errorController.js
@@ -1,9 +1,26 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    errro: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 
+const sendErrorPro = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'developement') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorPro(err, res);
+  }
 };
