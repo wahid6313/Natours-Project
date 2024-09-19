32e2315fe8e236142b969f3a3866af96e963c15e
wahid6313 <wahidali8566@gmail.com>
2024-09-19 11:29:57 +0530

    Password Managing

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,8 +1,11 @@
-const express = require("express");
-const userController = require("./../Controller/userController");
+const express = require('express');
+const userController = require('./../Controller/userController');
+const authController = require('./../Controller/authController');
 
 const router = express.Router();
 
-router.route("/").get(userController.getAllUsers);
+router.post('/signUp', authController.signUp);
+
+router.route('/').get(userController.getAllUsers);
 
 module.exports = router;
