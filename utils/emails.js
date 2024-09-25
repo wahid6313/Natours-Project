1ef1542038bc351c2ec8e752189c8ee66afe4b09
wahid6313 <wahidali8566@gmail.com>
2024-09-25 23:19:45 +0530

    Authorization-User ROle and Permission

--- utils/emails.js
+++ utils/emails.js
@@ -1,15 +1,15 @@
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
-// const sendEmail = (options) => {
-//   //create a transporter--
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD,
-//     },
-//   });
+const sendEmail = (options) => {
+  //create a transporter--
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-//   //define the email options--
-// };
+  //define the email options--
+};
