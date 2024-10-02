3356dd73647f57089c32a701641ecd849fef5ba7
Wahid Ali <wahidali@Wahids-MacBook-Air.local>
2024-10-02 16:44:23 +0530

    authorization

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,53 +1,53 @@
 // const { promisify } = require('util');
-// const jwt = require('jsonwebtoken');
-// const User = require('./../models/userModel');
-// const catchAsync = require('./../utils/catchAsync');
-// const AppError = require('./../utils/appError');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 // const sendEmail = require('../utils/emails');
 // const crypto = require('crypto');
 
-// const signToken = (id) => {
-//   return jwt.sign({ id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRES_IN,
-//   });
-// };
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
-// exports.signUp = catchAsync(async (req, res, next) => {
-//   const newUser = await User.create(req.body);
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
 
-//   const token = signToken(newUser._id);
+  const token = signToken(newUser._id);
 
-//   res.status(201).json({
-//     status: 'success',
-//     token,
-//     data: {
-//       user: newUser,
-//     },
-//   });
-// });
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
 
-// exports.login = catchAsync(async (req, res, next) => {
-//   const { email, password } = req.body;
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
 
-//   //check if email and password exists--
-//   if (!email || !password) {
-//     return next(new AppError('please provide email and password', 400));
-//   }
+  //check if email and password exists--
+  if (!email || !password) {
+    return next(new AppError('please provide email and password', 400));
+  }
 
-//   //check if user exist and password is corect--
-//   const user = await User.findOne({ email }).select('+password');
+  //check if user exist and password is corect--
+  const user = await User.findOne({ email }).select('+password');
 
-//   if (!user || !(await user.corectPassword(password, user.password))) {
-//     return next(new AppError('Incorrect user and Password', 401)); //401--unauthorized
-//   }
+  if (!user || !(await user.corectPassword(password, user.password))) {
+    return next(new AppError('Incorrect user and Password', 401)); //401--unauthorized
+  }
 
-//   //if everyone is okay, send token to clients--
-//   const token = signToken(user._id);
-//   res.status(200).json({
-//     status: 'success',
-//     token,
-//   });
-// });
+  //if everyone is okay, send token to clients--
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
 
 // exports.protect = catchAsync(async (req, res, next) => {
 //   //getting token and check of it's there --
--- models/userModel.js
+++ models/userModel.js
@@ -1,54 +1,54 @@
 // const crypto = require('crypto');
-// const mongoose = require('mongoose');
-// const validator = require('validator');
-// const bcrypt = require('bcryptjs');
-// const userSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, 'Enter your name'],
-//   },
-//   email: {
-//     type: String,
-//     required: [true, 'Enter your valid email'],
-//     unique: true,
-//     trim: true,
-//     lowercase: true,
-//     validate: [validator.isEmail, 'please provide a valid email'],
-//   },
-//   photo: String,
-//   role: {
-//     type: String,
-//     enum: ['user', 'guide', 'lead-guide', 'admin'],
-//     default: 'user',
-//   },
-//   password: {
-//     type: String,
-//     required: [true, 'Enter your valid password'],
-//     minlength: 8,
-//     select: false,
-//   },
-//   passwordConfirm: {
-//     type: String,
-//     required: [true, 'Enter a valid password'],
-//     validate: {
-//       validator: function (el) {
-//         return el === this.password;
-//       },
-//       message: 'password are not same',
-//     },
-//   },
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Enter your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Enter your valid email'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'please provide a valid email'],
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Enter your valid password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Enter a valid password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'password are not same',
+    },
+  },
 //   passwordChangedAt: Date,
 //   passwordResetToken: String,
 //   passwordResetExpires: Date,
-// });
+});
 
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
 
-//   this.password = await bcrypt.hash(this.password, 16);
-//   this.passwordConfirm = undefined;
-//   next();
-// });
+  this.password = await bcrypt.hash(this.password, 16);
+  this.passwordConfirm = undefined;
+  next();
+});
 
 // userSchema.methods.corectPassword = async function (
 //   candidatePassword,
@@ -76,6 +76,6 @@
 //   return resetToken;
 // };
 
-// const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-// module.exports = User;
+module.exports = User;
